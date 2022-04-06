781162bd664b0528ac3f35143f8909a1e761f4bc
JayDrojas <damianxrojas@gmail.com>
2022-04-06 16:53:28 -0700

    full crud for pictures, mild stylingas well

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -8,7 +8,7 @@ function LoginFormModal() {
 
   return (
     <>
-      <button onClick={() => setShowModal(true)}>Log In</button>
+      <button className="submit-bttn" onClick={() => setShowModal(true)}>Log In</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <LoginForm />
